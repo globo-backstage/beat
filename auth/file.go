7b420889149d1a2511b278fffd7ef38dbb51c46c
Wilson Júnior <wilsonpjunior@gmail.com>
2015-12-14 19:32:12 -0200

    Refactor: simplify token config with main config file
    Signed-off-by: Wilson Júnior <wilsonpjunior@gmail.com>

--- auth/file.go
+++ auth/file.go
@@ -1,21 +1,18 @@
 package auth
 
 import (
-	"fmt"
 	"io/ioutil"
 	"net/http"
 	"gopkg.in/yaml.v2"
 )
 
-
 type FileAuthentication struct {
-	tokens map[string]interface{}
+	Tokens map[string]*FileUser `yaml:"tokens"`
 }
 
 type FileUser struct {
-	data map[interface{}]interface{}
+	TokenEmail string `yaml:"email"`
 }
-
 
 func NewFileAuthentication(tokensPath string) (Authable, error) {
 	data, err := ioutil.ReadFile(tokensPath)
@@ -25,36 +22,21 @@ func NewFileAuthentication(tokensPath string) (Authable, error) {
 	}
 
 	authentication := &FileAuthentication{}
-	err = yaml.Unmarshal(data, &authentication.tokens)
+	err = yaml.Unmarshal(data, authentication)
 
-	fmt.Println(authentication.tokens)
+	if (err != nil) {
+		return nil, err
+	}
+
 	return authentication, nil
-	
+
 }
 
 func (authenticaton *FileAuthentication) GetUser(header *http.Header) User {
 	token := header.Get("Token")
-	tokenData := authenticaton.tokens[token]
-	
-	if tokenData == nil {
-		return nil
-	}
-
-	data, ok := tokenData.(map[interface{}]interface{})
-
-	if (!ok) {
-		return nil
-	}
-	
-	return &FileUser{data: data};
+	return authenticaton.Tokens[token]
 }
 
-func (user *FileUser) Email() string  {
-	email, ok := user.data["email"].(string)
-	if (ok) {
-		return email
-	} else {
-		return ""
-	}
+func (user FileUser) Email() string  {
+	return user.TokenEmail
 }
-
--- auth/file_test.go
+++ auth/file_test.go
@@ -14,16 +14,32 @@ func Test(t *testing.T) {
 	check.TestingT(t)
 }
 
-func (s *S) TestFileAuthenticationWith(c *check.C) {
-	a, err := NewFileAuthentication("../examples/tokens.yml")
+func (s *S) TestFileAuthenticationWithUserFound(c *check.C) {
+	a, err := NewFileAuthentication("../examples/config.yml")
 	c.Assert(err, check.IsNil)
-	
+
 	header := &http.Header{}
 	header.Set("Token", "example1")
-	
 
 	user := a.GetUser(header)
 	c.Assert(user, check.Not(check.IsNil))
-	
 	c.Assert(user.Email(), check.Equals, "admin@example.net")
+
+	header = &http.Header{}
+	header.Set("Token", "example2")
+
+	user = a.GetUser(header)
+	c.Assert(user, check.Not(check.IsNil))
+	c.Assert(user.Email(), check.Equals, "guest@example.net")
+}
+
+func (s *S) TestFileAuthenticationWithUserNotFound(c *check.C) {
+	a, err := NewFileAuthentication("../examples/config.yml")
+	c.Assert(err, check.IsNil)
+
+	header := &http.Header{}
+	header.Set("Token", "not-found")
+
+	user := a.GetUser(header)
+	c.Assert(user, check.IsNil)
 }
